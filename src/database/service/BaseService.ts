33f59b25782d1df4d2b445d5be2053574c226ec9
diegodisant <developerdiego0@gmail.com>
2024-05-09 09:45:00 -0600

    feat: implement movie service

---
+++ src/database/service/BaseService.ts
@@ -0,0 +1,34 @@
+import { BaseEntity } from "typeorm";
+import { IBaseRepository } from "../repository/BaseRepository";
+
+export interface IBaseService<Entity extends BaseEntity> {
+  getAll(): Promise<Entity[]>;
+  getOne(id: number): Promise<Entity>;
+  add(item: Entity): Promise<Entity>;
+  edit(id: number, item: Entity): Promise<Entity>;
+  remove(id: number): Promise<boolean>;
+}
+
+export abstract class BaseService<Entity extends BaseEntity> implements IBaseService<Entity> {
+  constructor(private repository: IBaseRepository<Entity>) {}
+
+  async getAll(): Promise<Entity[]> {
+    return await this.repository.all();
+  }
+
+  async getOne(id: number): Promise<Entity> {
+    return await this.repository.one(id);
+  }
+
+  async add(item: Entity): Promise<Entity> {
+    return await this.repository.create(item);
+  }
+
+  async edit(id: number, item: Entity): Promise<Entity> {
+    return await this.repository.update(id, item);
+  }
+
+  async remove(id: number): Promise<boolean> {
+    return await this.repository.delete(id);
+  }
+}
---
+++ src/database/service/MovieService.ts
@@ -0,0 +1,9 @@
+import { Movie } from "../entity/Movie";
+import { MovieRepository } from "../repository/MovieRepository";
+import { BaseService } from "./BaseService";
+
+export class MovieService extends BaseService<Movie> {
+  constructor() {
+    super(new MovieRepository());
+  }
+}
