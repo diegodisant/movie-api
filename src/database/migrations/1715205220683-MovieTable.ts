304cf2f26684dc5ae61300295a58484c8db88522
diegodisant <developerdiego0@gmail.com>
2024-05-09 09:42:39 -0600

    db: create movie table migration

---
+++ src/database/migrations/1715205220683-MovieTable.ts
@@ -0,0 +1,21 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class MovieTable1715205220683 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+   await queryRunner.query(`
+      CREATE TABLE IF NOT EXISTS movie (
+        id SERIAL PRIMARY KEY,
+        title VARCHAR(100) NOT NULL,
+        description VARCHAR(250) NOT NULL,
+        release_year CHAR(4) NULL,
+        actors TEXT[],
+        created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+        updated_at TIMESTAMP NOT NULL DEFAULT NOW()
+      );
+    `);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('movie');
+  }
+}
