dbfd70dc752093a42854ec0a37203cfc05a8817b
diegodisant <developerdiego0@gmail.com>
2024-05-12 10:20:46 -0600

    feat: implement class-validator in Movie entity

--- src/database/entity/Movie.ts
+++ src/database/entity/Movie.ts
@@ -7,21 +7,38 @@ import {
   UpdateDateColumn
 } from 'typeorm';
 
+import {
+ IsInt,
+ IsString,
+ IsArray,
+ IsOptional,
+ Length
+} from 'class-validator';
+
 @Entity()
 export class Movie extends BaseEntity {
   @PrimaryGeneratedColumn()
+  @IsInt()
   id: number;
 
   @Column()
+  @IsString()
+  @Length(1, 255)
   title: string;
 
   @Column()
+  @IsString()
+  @Length(1, 255)
   description: string;
 
   @Column({ type: 'char', length: 4, nullable: true })
+  @IsString()
+  @Length(4, 4)
+  @IsOptional()
   release_year?: string;
 
   @Column({ type: 'text', array: true})
+  @IsArray()
   actors: string[];
 
   @CreateDateColumn({ name: 'created_at'})
